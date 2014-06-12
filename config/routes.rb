0869b03f1eec713dfc4834018e9fe67ec0109858
Sharanya Bathey <sharanya.bathey@gmail.com>
2014-06-12 10:58:05 +0530

    #130 - Changing routes to new Rails 4 format.
    In rails 2 - :as option lets you override the normal naming for the actual generated paths.
    In rails 3 - :as option lets you override the normal naming for the named route helpers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-
+Rails.application.routes.draw do
   admin_prefix= ENV['ADMIN_OAUTH_URL_PREFIX']
   user_prefix= ENV['USER_OAUTH_URL_PREFIX']
 
@@ -9,13 +8,18 @@ ActionController::Routing::Routes.draw do |map|
   admin_prefix = admin_prefix.gsub(%r{^/}, '').gsub(%r{/$}, '')
   user_prefix = user_prefix.gsub(%r{^/}, '').gsub(%r{/$}, '')
 
-  map.connect "#{user_prefix}/gadgets/oauthcallback", :controller => "gadget_oauth_callback", :action => 'oauth_callback'
+  get "#{user_prefix}/gadgets/oauthcallback" => "gadget_oauth_callback#oauth_callback"
+  get "#{user_prefix}/gadgets/ifr" => "gadget_rendering#ifr", as: :gadget_rendering
+  get "#{user_prefix}/gadgets/makeRequest" => "gadget_make_request#make_request"
+  get "#{user_prefix}/gadgets/proxy" => "gadget_proxy#proxy"
+  get "#{user_prefix}/gadgets/concat" => "gadget_proxy#concat"
+  get "#{user_prefix}/gadgets/js/:features.js" => "gadget_js_request#js"
 
-  map.gadget_rendering "#{user_prefix}/gadgets/ifr", :controller => "gadget_rendering", :action => 'ifr'
-  map.connect "#{user_prefix}/gadgets/makeRequest", :controller => "gadget_make_request", :action => 'make_request'
-  map.connect "#{user_prefix}/gadgets/proxy", :controller => "gadget_proxy", :action => 'proxy'
-  map.connect "#{user_prefix}/gadgets/concat", :controller => "gadget_proxy", :action => 'concat'
-  map.connect "#{user_prefix}/gadgets/js/:features.js", :controller => 'gadget_js_request', :action => 'js'
-
-  map.resources :gadgets_oauth_clients, :controller => 'gadgets_oauth_clients', :as => admin_prefix + (admin_prefix.blank? ? "" : "/") + "gadgets/oauth_clients"
+  gadget_oauth_client_route = admin_prefix + (admin_prefix.blank? ? "" : "/") + "gadgets/oauth_clients"
+  get "#{gadget_oauth_client_route}" => "gadgets_oauth_clients#index"
+  get "#{gadget_oauth_client_route}/new" => "gadgets_oauth_clients#new"
+  post "#{gadget_oauth_client_route}" => "gadgets_oauth_clients#create"
+  get "#{gadget_oauth_client_route}/:id/edit" => "gadgets_oauth_clients#edit"
+  put "#{gadget_oauth_client_route}/:id" => "gadgets_oauth_clients#update"
+  delete "#{gadget_oauth_client_route}/:id" => "gadgets_oauth_clients#destroy"
 end
